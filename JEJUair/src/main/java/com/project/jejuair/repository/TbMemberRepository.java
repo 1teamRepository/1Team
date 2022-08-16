b99c4705428ac9cdcda5f120524841a8d6e27d6d
임성호 <shlim2210@naver.com>
2022-08-16 21:30:45 +0900

    성호_수정 완료_0816

--- JEJUair/src/main/java/com/project/jejuair/repository/TbMemberRepository.java
+++ JEJUair/src/main/java/com/project/jejuair/repository/TbMemberRepository.java
@@ -1,6 +1,5 @@
 package com.project.jejuair.repository;
 
-import com.project.jejuair.model.entity.TbExtraService;
 import com.project.jejuair.model.entity.TbMember;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
